d168fecdc9431362fb950a5236c45b26f86c592d
Sanu Vishwakarma <sanuvish1920@gmail.com>
2026-06-20 13:48:16 +0530

    First Project

---
+++ PR. 1 Fundamental Booster.py
@@ -0,0 +1,34 @@
+# Personal Data Collector
+
+print("Welcome to the Interactive Personal Data Collector!\n")
+
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+height = float(input("Please enter your height in feet: "))
+fav_number = int(input("Please enter your favourite number: "))
+
+print("\nThank you! Here is the information we collected:\n")
+
+print("Name:", name,
+      "(Type:", type(name),
+      ", Memory Address:", id(name), ")")
+
+print("Age:", age,
+      "(Type:", type(age),
+      ", Memory Address:", id(age), ")")
+
+print("Height:", height,
+      "(Type:", type(height),
+      ", Memory Address:", id(height), ")")
+
+print("Favourite Number:", fav_number,
+      "(Type:", type(fav_number),
+      ", Memory Address:", id(fav_number), ")")
+
+birth_year = 2026 - age
+
+print("\nYour birth year is approximately:",
+      birth_year,
+      "(based on your age of", age, ")")
+
+print("\nThank you for using the Personal Data Collector. Goodbye!")
